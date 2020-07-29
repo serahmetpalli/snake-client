bad752e2db19e2ed420a8ca63ebd50cca5eb836a
Serah Sirohi <serahsirohi@gmail.com>
2020-07-29 21:22:17 +0000

    adding updated play file

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function(callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", callback)
+  return stdin;
+}
+
+const handleUserInput = function (data){
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput(handleUserInput);
